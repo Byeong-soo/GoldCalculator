4fcf564a6dfae6b3904fb0e8850e387e23526d2c
byeongSoo <ubs4939@naver.com>
2022-03-28 20:25:32 +0900

    금액 천단위 콤마 및 소수점 생성

---
+++ app/src/main/java/com/goldcalculator/CustomTextWatcher.java
@@ -0,0 +1,103 @@
+package com.goldcalculator;
+
+import android.text.Editable;
+import android.text.Selection;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+
+import java.text.DecimalFormat;
+import java.util.StringTokenizer;
+
+public class CustomTextWatcher implements TextWatcher {
+
+    private EditText editText;
+
+    CustomTextWatcher(EditText editText) {
+        this.editText = editText;
+    }
+
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+        try {
+            editText.removeTextChangedListener(this);
+            String value = editText.getText().toString();
+
+            if (value != null & !value.equals("")) {
+                if (value.startsWith(".")) {
+                    editText.setText("0.");
+                }
+                if (value.startsWith("0") && !value.startsWith("0.")) {
+                    editText.setText("");
+                }
+                String str = editText.getText().toString().replaceAll(",", "");
+                if (!value.equals(""))
+                    editText.setText(getDecimalFormattedString(str));
+                editText.setSelection(editText.getText().toString().length());
+            }
+            editText.addTextChangedListener(this);
+            return;
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+            editText.addTextChangedListener(this);
+        }
+    }
+
+
+    protected String getDecimalFormattedString(String value) {    // 천단위 콤마설정.
+
+        StringTokenizer lst = new StringTokenizer(value, ".");
+        String str1 = value;
+        String str2 = "";
+
+        if (lst.countTokens() > 1) {
+            str1 = lst.nextToken();
+            str2 = lst.nextToken();
+        }
+
+        String str3 = "";
+        int i = 0;
+        int j = -1 + str1.length();
+        if (str1.charAt(-1 + str1.length()) == '.') {
+            j--;
+            str3 = ".";
+        }
+
+        for (int k = j; ; k--) {
+            if (k < 0) {
+                if (str2.length() > 0)
+                    str3 = str3 + "." + str2;
+                return str3;
+            }
+
+            if (i == 3) {
+                str3 = "," + str3;
+                i = 0;
+            }
+            str3 = str1.charAt(k) + str3;
+            i++;
+        }
+    }
+
+    public static String trimCommonOfString(String string) {
+        if (string.contains(",")) {
+            return string.replace(",", "");
+        } else {
+            return string;
+        }
+    }
+
+}
